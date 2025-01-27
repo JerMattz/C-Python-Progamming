b45f16f757644ae0e265af6993be841864738976
JerMattz <96379523+JerMattz@users.noreply.github.com>
2025-01-27 12:26:56 +0530

    Python-Programming

---
+++ Python-Programming/main.py
@@ -0,0 +1,12 @@
+# Tokens Of Python
+# -Keywords
+# -Operators
+# -Identifiers / Variable Names
+# -Literals / Datatypes
+# -Delimiters
+
+for i in range(10):
+    if (i < 9):
+        print("Hello world!")
+    else:
+        print("Hello")
